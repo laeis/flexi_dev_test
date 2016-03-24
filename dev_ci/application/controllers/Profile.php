bcc5a27f1c9f58efef32cb338786213c2346842d
laeis <webartiko@gmail.com>
2016-03-24 16:44:08 +0200

    edit profile form

--- dev_ci/application/controllers/Profile.php
+++ dev_ci/application/controllers/Profile.php
@@ -27,9 +27,10 @@ class Profile extends CI_Controller {
 		if( ! isset( $this->session->userdata['logged_in'] ) ) {
 			redirect('/');
 		}
+		var_dump( $this->input->post() );
 		$user_id = $this->session->userdata['logged_in']['user_ID'];
 		$profile_data = $this->User_profile_model->getProfile( $user_id );
-		var_dump($profile_data);
+		//var_dump($profile_data);
 		$this->load->view( 'header', $profile_data );
 		$this->load->view( 'user/profile/profile' );
 		$this->load->view( 'footer' );
--- dev_ci/application/views/user/profile/profile.php
+++ dev_ci/application/views/user/profile/profile.php
@@ -1,109 +1,112 @@
 
 <div class="container">
-		<form class="form-horizontal">
-			<div class="form-group">
-				<h3> Name : </h3>
-			</div>
-			<div class="form-group">
-				<label for="inputEmail3" class="col-sm-3 control-label">
+	<?php $attributes = array( "name" => "profileUserForm" , "class" => "form-horizontal profile-user-form", "id" =>"profile_user_form" ); 
+	echo form_open( "user/profile", $attributes ); ?>
+		<div class="form-group">
+			<h3> Name : </h3>
+		</div>
+		<div class="form-group">
+			<label for="input_username" class="col-sm-3 control-label">
 Username</label>
-				<div class="col-sm-9">
-					<input type="email" class="form-control" id="inputEmail3" placeholder="Email" readonly>
-				</div>
+			<div class="col-sm-9">
+				<input id="input_email" type="email" class="form-control" placeholder="Username" readonly>
+				<p class="help-block"> Usernames cannot be changed. </p>
 			</div>
-			<div class="form-group">
-				<label for="inputPassword3" class="col-sm-3 control-label">First Name</label>
-				<div class="col-sm-9">
-					<input type="text" class="form-control" id="inputPassword3" placeholder="Password">
-				</div>
+		</div>
+		<div class="form-group">
+			<label for="input_firstname" class="col-sm-3 control-label">First Name</label>
+			<div class="col-sm-9">
+				<input id="input_firstname" type="text" name="input-firstname" class="form-control" placeholder="First Name" value="<?php echo !empty( form_error( 'input-firstname' ) ) ? form_error( 'input-firstname' ) : $firstname; ?>">
 			</div>
-			<div class="form-group">
-				<label for="inputPassword3" class="col-sm-3 control-label">Last Name</label>
-				<div class="col-sm-9">
-					<input type="text" class="form-control" id="inputPassword3" placeholder="Password">
-				</div>
+		</div>
+		<div class="form-group">
+			<label for="input_lastname" class="col-sm-3 control-label">Last Name</label>
+			<div class="col-sm-9">
+				<input id="input_lastname" type="text" class="form-control" name="input-lastname" placeholder="Last Name" value="<?php echo !empty( form_error( 'password' ) ) ? form_error( 'password' ) : ''; ?>">
 			</div>
-			<div class="form-group">
-				<label for="inputPassword3" class="col-sm-3 control-label">Display name publicly as</label>
-				<div class="col-sm-9">
-					<select name="display_name" id="display_name" class="form-control">
-					<?php
-						$public_display = array();
-						$public_display['display_username']  = $display_name;
+		</div>
+		<div class="form-group">
+			<label for="input_display_name" class="col-sm-3 control-label">Display name publicly as</label>
+			<div class="col-sm-9">
+				<select id="input_display_name" name="input-display-name" class="form-control">
+				<?php
+					$public_display = array();
+					$public_display['display_username']  = $display_name;
 
-						if ( !empty($profileuser->first_name) )
-							$public_display['display_firstname'] = $firstname;
+					if ( !empty( $firstname ) )
+						$public_display['display_firstname'] = $firstname;
 
-						if ( !empty($profileuser->last_name) )
-							$public_display['display_lastname'] = $lastname;
+					if ( !empty( $lastname ) )
+						$public_display['display_lastname'] = $lastname;
 
-						if ( !empty($profileuser->first_name) && !empty($profileuser->last_name) ) {
-							$public_display['display_firstlast'] = $firstname . ' ' . $lastname;
-							$public_display['display_lastfirst'] = $lastname . ' ' . $firstname;
-						}
+					if ( !empty( $firstname ) && !empty( $lastname ) ) {
+						$public_display['display_firstlast'] = $firstname . ' ' . $lastname;
+						$public_display['display_lastfirst'] = $lastname . ' ' . $firstname;
+					}
 
-						$public_display = array_map( 'trim', $public_display );
-						$public_display = array_unique( $public_display );
+					$public_display = array_map( 'trim', $public_display );
 
-						foreach ( $public_display as $id => $item ) { ?>
-							<option <?php ?> ><?php echo $item; ?></option>
-						<?php } ?>
-					</select>
-				</div>
+					foreach ( $public_display as $id => $item ) { ?>
+						<option <?php ?> ><?php echo $item; ?></option>
+					<?php } ?>
+				</select>
 			</div>
-			<div class="form-group">
-				<h3> Contact Info : </h3>
-			</div>
-			<div class="form-group">
-				<label for="inputPassword3" class="col-sm-3 control-label">
+		</div>
+		<div class="form-group">
+			<h3> Contact Info : </h3>
+		</div>
+		<div class="form-group">
+			<label for="input_email" class="col-sm-3 control-label">
 Email (required)</label>
-				<div class="col-sm-9">
-					<input type="text" class="form-control" id="inputPassword3" placeholder="Password">
-				</div>
+			<div class="col-sm-9">
+				<input id="input_email" type="email" class="form-control" name="input-email" placeholder="Email">
 			</div>
-			<div class="form-group">
-				<label for="inputPassword3" class="col-sm-3 control-label">Website</label>
-				<div class="col-sm-9">
-					<input type="text" class="form-control" id="inputPassword3" placeholder="Password">
-				</div>
+		</div>
+		<div class="form-group">
+			<label for="input_website" class="col-sm-3 control-label">Website</label>
+			<div class="col-sm-9">
+				<input id="input_website" type="text" class="form-control" name="input-website" placeholder="Website">
 			</div>
-			<div class="form-group">
-				<h3> About Yourself : </h3>
-			</div>
-			<div class="form-group">
-				<label for="inputPassword3" class="col-sm-3 control-label">
+		</div>
+		<div class="form-group">
+			<h3> About Yourself : </h3>
+		</div>
+		<div class="form-group">
+			<label for="input_description" class="col-sm-3 control-label">
 Biographical Info</label>
-				<div class="col-sm-9">
-					<input type="text" class="form-control" id="inputPassword3" placeholder="Password">
-				</div>
+			<div class="col-sm-9">
+				<textarea  id="input_description"  name="input-description" class="form-control" placeholder="Biographical Info" ></textarea>
 			</div>
-			<div class="form-group">
-				<label for="inputPassword3" class="col-sm-3 control-label">Profile Picture</label>
-				<div class="col-sm-9">
-					<input type="text" class="form-control" id="inputPassword3" placeholder="Password">
-				</div>
+		</div>
+		<div class="form-group">
+			<label for="input_avatar" class="col-sm-3 control-label">Profile Picture</label>
+			<div class="col-sm-9">
+				<img id="avatar_img" src="" >
+				<button id="input_avatar" type="button" class="btn btn-primary">Upload Picture</button>
+				<input type="hidden" name="avatar-img-id" value="" > 
 			</div>
-			<div class="form-group">
-				<h3> Account Management :</h3>
-			</div>
-			<div class="form-group">
-				<label for="inputPassword3" class="col-sm-3 control-label">
+		</div>
+		<div class="form-group">
+			<h3> Account Management :</h3>
+		</div>
+		<div class="form-group">
+			<label for="input_password" class="col-sm-3 control-label">
 New Password</label>
-				<div class="col-sm-9">
-					<input type="text" class="form-control" id="inputPassword3" placeholder="Password">
-				</div>
+			<div class="col-sm-9">
+				<input  id="input_password"  type="password" class="form-control" name="input-password" placeholder="New Password">
 			</div>
-			<div class="form-group">
-				<label for="inputPassword3" class="col-sm-3 control-label">
+		</div>
+		<div class="form-group">
+			<label for="input_cpassword" class="col-sm-3 control-label">
 Confirm Password</label>
-				<div class="col-sm-9">
-					<input type="text" class="form-control" id="inputPassword3" placeholder="Password">
-				</div>
+			<div class="col-sm-9">
+				<input id="input_cpassword" type="password" class="form-control" name="input-cpassword" placeholder="Confirm Password">
 			</div>
-			<div class="form-group">
-				<div class="col-sm-12">
-					<button type="submit" class="btn btn-primary">Update Profile</button>
-				</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-12">
+				<button name="update-profile" type="submit" class="btn btn-primary">Update Profile</button>
 			</div>
-		</form>
+		</div>
+	<?php echo form_close(); ?>
 </div>
