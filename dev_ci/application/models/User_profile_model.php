8b775c46b9ee17177cd229bbe5cc87ddbe543957
laeis <webartiko@gmail.com>
2016-03-25 02:09:00 +0200

    create model for save and edit profile data, edit profile conroller and view

--- dev_ci/application/models/User_profile_model.php
+++ dev_ci/application/models/User_profile_model.php
@@ -13,17 +13,51 @@ class User_profile_model extends CI_Model
 
     public function getProfile( $id ){
 
-    	$this->db->select( 'u1.id, u1.user_login, u1.user_pass, u1.user_email, u1.user_url, u1.display_name, m1.meta_value AS firstname, m2.meta_value AS lastname, m3.meta_value AS description, m4.meta_value AS avatar_image' );
+    	$this->db->select( 'u1.id, u1.user_login, u1.user_nicename, u1.user_pass, u1.user_email, u1.user_url, u1.display_name, m1.meta_value AS firstname, m2.meta_value AS lastname, m3.meta_value AS description, m4.meta_value AS avatar_image_id, m5.guid AS avatar_image_url' );
 		$this->db->from( 'users as u1' );
 		$this->db->join( 'usermeta as m1', "m1.user_id = u1.id AND m1.meta_key = 'first_name'", 'left');
 		$this->db->join( 'usermeta as m2', "m2.user_id = u1.id AND m2.meta_key = 'last_name'", 'left');
 		$this->db->join( 'usermeta as m3', "m3.user_id = u1.id AND m3.meta_key = 'description'", 'left');
-		$this->db->join( 'usermeta as m4', "m3.user_id = u1.id AND m3.meta_key = 'avatar_image'", 'left');
-		$this->db->where( 'ID', $id );
+		$this->db->join( 'usermeta as m4', "m4.user_id = u1.id AND m4.meta_key = 'avatar_image'", 'left');
+		$this->db->join( 'posts as m5', "m5.id = m4.meta_value AND m5.post_type = 'attachment'", 'left');
+		$this->db->where( 'u1.ID', $id );
 		return $this->db->get()->row();
     }
 
-    public function updateProfile( $id ){
+    public function updateProfile( $id, $data ){
 
+    	$this->db->set( $data );
+		$this->db->where( 'ID', $id );
+		return $this->db->update( 'users' ); 
+    }
+
+    public function updateProfileOption( $id, $option ){
+
+    	$data_metakey = array();
+    	$options_data = $this->db->select( 'meta_key' )->where( 'user_id', $id )->get( 'usermeta' )->result_array();
+    	if( is_array( $options_data ) || is_object( $options_data ) ) {
+	    	foreach ( $options_data as $key => $option_data ) {
+	    		$data_metakey[] = $option_data['meta_key'];
+	    	}
+	    }
+	   	if( ! empty( $data_metakey ) ){
+	   		if( is_array( $option ) || is_object( $option ) ){
+	    		foreach ( $option as $key => $value ) {
+		    		if( in_array( $value['meta_key'], $data_metakey ) ){
+		    			$this->db->set( 'meta_value', $value['meta_value'] );
+		    			$this->db->where( 'meta_key', $value['meta_key'] );
+						$this->db->where( 'user_id', $id );
+						$this->db->update( 'usermeta' );
+		    		} else {
+						$this->db->insert( 'usermeta', $value );
+		    		}
+		    	}
+		    	return true;	
+	    	} 
+	   	} else {
+	   		return $this->db->insert_batch( 'usermeta', $option );
+	   	}
+
+    	return false;
     }
 }
--- dev_ci/application/views/user/profile/profile.php
+++ dev_ci/application/views/user/profile/profile.php
@@ -1,28 +1,44 @@
 
 <div class="container">
+	<div class="row" >
+		<div class="col-xs-12 col-sm-10 col-sm-offset-3 col-md-6 col-md-offset-3">
+			<p><?php echo $this->session->flashdata('msg'); ?></p>
+		</div>
+	</div>
 	<?php $attributes = array( "name" => "profileUserForm" , "class" => "form-horizontal profile-user-form", "id" =>"profile_user_form" ); 
 	echo form_open( "user/profile", $attributes ); ?>
 		<div class="form-group">
-			<h3> Name : </h3>
+			<div class="col-sm-9">
+				<h3> Name : </h3>
+			</div>
 		</div>
 		<div class="form-group">
 			<label for="input_username" class="col-sm-3 control-label">
 Username</label>
 			<div class="col-sm-9">
-				<input id="input_email" type="email" class="form-control" placeholder="Username" readonly>
+				<input id="input_email" type="email" class="form-control" placeholder="Username" value="<?php echo $user_login; ?>"readonly>
 				<p class="help-block"> Usernames cannot be changed. </p>
 			</div>
 		</div>
 		<div class="form-group">
 			<label for="input_firstname" class="col-sm-3 control-label">First Name</label>
 			<div class="col-sm-9">
-				<input id="input_firstname" type="text" name="input-firstname" class="form-control" placeholder="First Name" value="<?php echo !empty( form_error( 'input-firstname' ) ) ? form_error( 'input-firstname' ) : $firstname; ?>">
+				<input id="input_firstname" type="text" name="input-firstname" class="form-control" placeholder="First Name" value="<?php echo !empty( $this->input->post( 'input-firstname' ) ) ? $this->input->post( 'input-firstname' ) : $firstname; ?>">
+				<span class="text-danger"><?php echo form_error( 'input-firstname' ); ?></span>
 			</div>
 		</div>
 		<div class="form-group">
 			<label for="input_lastname" class="col-sm-3 control-label">Last Name</label>
 			<div class="col-sm-9">
-				<input id="input_lastname" type="text" class="form-control" name="input-lastname" placeholder="Last Name" value="<?php echo !empty( form_error( 'password' ) ) ? form_error( 'password' ) : ''; ?>">
+				<input id="input_lastname" type="text" class="form-control" name="input-lastname" placeholder="Last Name" value="<?php echo !empty( $this->input->post( 'input-lastname' ) ) ? $this->input->post( 'input-lastname' ) : $lastname; ?>">
+				<span class="text-danger"><?php echo form_error( 'input-lastname' ); ?></span>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="input_nicename" class="col-sm-3 control-label">Nickname (required)</label>
+			<div class="col-sm-9">
+				<input id="input_nicename" type="text" class="form-control" name="input-nicename" placeholder="Nickname" value="<?php echo !empty( $this->input->post( 'input-nicename' ) ) ? $this->input->post( 'input-nicename' ) : $user_nicename; ?>">
+				<span class="text-danger"><?php echo form_error( 'input-nicename' ); ?></span>
 			</div>
 		</div>
 		<div class="form-group">
@@ -31,7 +47,8 @@ Username</label>
 				<select id="input_display_name" name="input-display-name" class="form-control">
 				<?php
 					$public_display = array();
-					$public_display['display_username']  = $display_name;
+					$public_display['display_username']  = $user_login;
+					$public_display['display_display_name']  = $display_name;
 
 					if ( !empty( $firstname ) )
 						$public_display['display_firstname'] = $firstname;
@@ -45,11 +62,12 @@ Username</label>
 					}
 
 					$public_display = array_map( 'trim', $public_display );
-
-					foreach ( $public_display as $id => $item ) { ?>
-						<option <?php ?> ><?php echo $item; ?></option>
+					$public_display = array_unique( $public_display );
+					foreach ( $public_display as $item ) { ?>
+						<option <?php echo ( $display_name ==  $item ) ? 'selected="selected"' : ''; ?> ><?php echo $item; ?></option>
 					<?php } ?>
 				</select>
+				<span class="text-danger"><?php echo form_error( 'input-display-name' ); ?></span>
 			</div>
 		</div>
 		<div class="form-group">
@@ -59,13 +77,15 @@ Username</label>
 			<label for="input_email" class="col-sm-3 control-label">
 Email (required)</label>
 			<div class="col-sm-9">
-				<input id="input_email" type="email" class="form-control" name="input-email" placeholder="Email">
+				<input id="input_email" type="email" class="form-control" name="input-email" placeholder="Email" value="<?php echo !empty( $this->input->post( 'input-email' ) ) ? $this->input->post( 'input-email' ) : $user_email ; ?>">
+				<span class="text-danger"><?php echo form_error( 'input-email' ); ?></span>
 			</div>
 		</div>
 		<div class="form-group">
 			<label for="input_website" class="col-sm-3 control-label">Website</label>
 			<div class="col-sm-9">
-				<input id="input_website" type="text" class="form-control" name="input-website" placeholder="Website">
+				<input id="input_website" type="text" class="form-control" name="input-website" placeholder="Website" value="<?php echo !empty( $this->input->post( 'input-website' ) ) ? $this->input->post( 'input-website' ) : $user_url ; ?>">
+				<span class="text-danger"><?php echo form_error( 'input-website' ); ?></span>
 			</div>
 		</div>
 		<div class="form-group">
@@ -75,15 +95,17 @@ Email (required)</label>
 			<label for="input_description" class="col-sm-3 control-label">
 Biographical Info</label>
 			<div class="col-sm-9">
-				<textarea  id="input_description"  name="input-description" class="form-control" placeholder="Biographical Info" ></textarea>
+				<textarea  id="input_description"  name="input-description" class="form-control" placeholder="Biographical Info" ><?php echo !empty( $this->input->post( 'input-description' ) ) ? $this->input->post( 'input-description' ) : $description ; ?></textarea>
+				<span class="text-danger"><?php echo form_error( 'input-description' ); ?></span>
 			</div>
 		</div>
 		<div class="form-group">
 			<label for="input_avatar" class="col-sm-3 control-label">Profile Picture</label>
 			<div class="col-sm-9">
-				<img id="avatar_img" src="" >
+				<img id="avatar_img" src="<?php $avatar_image_url ?>" >
 				<button id="input_avatar" type="button" class="btn btn-primary">Upload Picture</button>
-				<input type="hidden" name="avatar-img-id" value="" > 
+				<input type="hidden" name="avatar-img-id" value="<?php echo !empty( $this->input->post( 'avatar-img-id' ) ) ? $this->input->post( 'avatar-img-id' ) : $avatar_image_id ; ?>" >
+				<span class="text-danger"><?php echo form_error( 'avatar-img-id' ); ?></span> 
 			</div>
 		</div>
 		<div class="form-group">
@@ -93,18 +115,21 @@ Biographical Info</label>
 			<label for="input_password" class="col-sm-3 control-label">
 New Password</label>
 			<div class="col-sm-9">
-				<input  id="input_password"  type="password" class="form-control" name="input-password" placeholder="New Password">
+				<input  id="input_password"  type="password" class="form-control" name="input-password" placeholder="New Password" value="">
+				<span class="text-danger"><?php echo form_error( 'input-password' ); ?></span>
 			</div>
 		</div>
 		<div class="form-group">
 			<label for="input_cpassword" class="col-sm-3 control-label">
 Confirm Password</label>
 			<div class="col-sm-9">
-				<input id="input_cpassword" type="password" class="form-control" name="input-cpassword" placeholder="Confirm Password">
+				<input id="input_cpassword" type="password" class="form-control" name="input-cpassword" placeholder="Confirm Password" value="">
+				<span class="text-danger"><?php echo form_error( 'input-cpassword' ); ?></span>
 			</div>
 		</div>
 		<div class="form-group">
 			<div class="col-sm-12">
+				<input type="hidden" name="user-id" value="<?php echo $id; ?>">
 				<button name="update-profile" type="submit" class="btn btn-primary">Update Profile</button>
 			</div>
 		</div>
